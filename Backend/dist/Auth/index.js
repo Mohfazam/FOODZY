181af3cf36a25dea9871a030cee81efb790230cf
Mohfazam <mohd.sarwar.code@gmail.com>
2025-11-03 04:42:01 +0530

    OTP verification

--- Backend/dist/Auth/index.js
+++ Backend/dist/Auth/index.js
@@ -34,4 +34,28 @@ exports.authHandler.post("/request-otp", async (req, res) => {
         return res.status(500).json({ message: "Internal server error" });
     }
 });
+exports.authHandler.post("/verify-otp", async (req, res) => {
+    const { email, otp } = req.body;
+    if (!email)
+        return res.status(400).json({ message: "Email is required" });
+    if (!otp)
+        return res.status(400).json({ message: "otp is required" });
+    try {
+        const existingOtp = await prisma.oTP.findFirst({ where: { email } });
+        if (!existingOtp) {
+            return res.status(400).json({ message: "Invalid email" });
+        }
+        if (existingOtp.code != otp) {
+            return res.status(400).json({ message: "Invalid OTP" });
+        }
+        if (new Date() > existingOtp.expiresAt) {
+            return res.status(400).json({ message: "OTP has expired" });
+        }
+        await prisma.oTP.delete({ where: { id: existingOtp.id } });
+        return res.status(200).json({ message: "OTP verified successfully" });
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+});
 //# sourceMappingURL=index.js.map
--- Backend/src/Auth/index.ts
+++ Backend/src/Auth/index.ts
@@ -18,7 +18,7 @@ authHandler.post("/request-otp", async (req, res) => {
     if (!email) return res.status(400).json({ message: "Email is required" });
 
     try{
-         const otpCode = Math.floor(100000 + Math.random() * 900000).toString();
+        const otpCode = Math.floor(100000 + Math.random() * 900000).toString();
         const expiresAt = new Date(Date.now() + 5 * 60 * 1000); 
 
         await prisma.oTP.deleteMany({ where: { email } });
@@ -37,5 +37,39 @@ authHandler.post("/request-otp", async (req, res) => {
     } catch(error){
         console.error("Error sending OTP:", error);
     return res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+authHandler.post("/verify-otp", async (req, res) => {
+    const {email, otp} = req.body;
+
+    if(!email) return res.status(400).json({ message: "Email is required" });
+    if(!otp) return res.status(400).json({ message: "otp is required" });
+
+    try{
+        const existingOtp = await prisma.oTP.findFirst({ where: { email } });
+
+        if (!existingOtp) {
+        return res.status(400).json({ message: "Invalid email" });
+        }
+
+        if (existingOtp.code != otp) {
+            return res.status(400).json({ message: "Invalid OTP" });
+        }
+
+
+        if (new Date() > existingOtp.expiresAt) {
+            return res.status(400).json({ message: "OTP has expired" });
+        }
+
+
+        
+        await prisma.oTP.delete({ where: { id: existingOtp.id } });
+        return res.status(200).json({ message: "OTP verified successfully" });
+
+
+    } catch(error){
+        
+        return res.status(500).json({ message: "Something went wrong" });
     }
 });

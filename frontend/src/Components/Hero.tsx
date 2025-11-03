c74104d31ef7ef1b4f794473c705031b7502964a
Mohfazam <mohd.sarwar.code@gmail.com>
2025-11-04 04:48:18 +0530

    Hero

---
+++ frontend/src/Components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="w-full h-[848px] m-0">
+  <img src="/Hero.svg" alt="Hero Image" className="w-full h-full object-cover" />
+</div>
+
+  )
+}
+
+export default Hero
+
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
-import {Navbar} from "@/Components/Navbar"
+import Hero from "@/Components/Hero";
+import { Navbar } from "@/Components/Navbar"
 
 export default function Home() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
   );
 }

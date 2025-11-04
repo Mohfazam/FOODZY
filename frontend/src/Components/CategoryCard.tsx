f2aa1dd2ffd69b65e1eb06b80dba5c31bd60c97a
Mohfazam <mohd.sarwar.code@gmail.com>
2025-11-04 20:29:15 +0530

    Category Card Mapping

--- frontend/src/Components/CategoryCard.tsx
+++ frontend/src/Components/CategoryCard.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+interface CategoryCardProps{
+  text: string;
+  image: string;
+}
 
-const CategoryCard = () => {
+const CategoryCard = ({text, image}: CategoryCardProps) => {
   return (
     <div className="w-[482px] h-[300px] rounded-[10px] relative overflow-hidden">
       <img
-        src="/Category-1.svg"
+        src={image}
         alt="Category1"
         className="w-full h-full object-cover absolute inset-0"
       />
@@ -13,7 +16,7 @@ const CategoryCard = () => {
       <div className="absolute inset-0 z-10 flex items-center pl-[50px]">
         <div className="flex flex-col gap-6">
           <span className="w-[203px] text-[24px] leading-6 text-[#253D4E] font-semibold">
-            Everyday Fresh & Clean With Our Products
+            {text}
           </span>
           <button className="bg-[#F53E32] text-white rounded-sm w-[103px] h-8 px-5 py-2 font-medium text-[12px] leading-[15px]">
             Shop Now
--- frontend/src/Components/CategoryGrid.tsx
+++ frontend/src/Components/CategoryGrid.tsx
@@ -1,11 +1,27 @@
 import CategoryCard from "./CategoryCard"
 
+const Cards = [
+  {
+    text : "Everyday Fresh & Clean with Our Products",
+    image: "/Category-1.svg"
+  },
+  {
+    text : "Make your Breakfast Healthy and Easy",
+    image: "/Category-2.svg"
+  },
+  {
+    text : "The best Organic Products Online",
+    image: "/Category-3.svg"
+  }
+]
+
 const CategoryGrid = () => {
   return (
     <div className="flex max-w-[1585.97px] gap-4 mx-auto px-4 py-8 sm:px-6 lg:px-8">
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
+      {Cards.map((e, i) => (
+        
+        <CategoryCard text={e.text} image={e.image} key={i}/>
+      ))}
     </div>
   )
 }

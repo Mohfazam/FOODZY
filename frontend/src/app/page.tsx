aebfb7a063af0926fd856cdc46e70b6df59a2953
Mohfazam <mohd.sarwar.code@gmail.com>
2025-11-05 02:48:17 +0530

    Products Cached

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,6 @@
 import CategoryGrid from "@/Components/CategoryGrid";
 import Hero from "@/Components/Hero";
 import { Navbar } from "@/Components/Navbar"
-import { useQuery } from '@tanstack/react-query'
-import { fetchProducts } from '@/lib/api'
 import ProductsWrapper from "@/Components/ProductsWrapper";
 
 export default function Home() {
